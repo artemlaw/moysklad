8b3a987fe393b97fca12458541b07962a9042ad1
Artem Lubentsov <artem.law@mail.ru>
2024-05-31 10:33:21 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=['requests', 'pymupdf', 'reportlab'],
+    package_data={'moysklad': ['Roboto-Bold.ttf']},
+    include_package_data=True,
     author='Lubentsov Artem',
     author_email='artem.law@mail.ru',
     description='Integration module MoySklad',
